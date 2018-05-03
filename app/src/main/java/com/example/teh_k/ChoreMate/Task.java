92ca4ea59df3b17e276a727fec5db5ffe7ebf42c
yutao <yutaohi@gmail.com>
2018-05-03 14:51:43 -0700

    Calender for time and comments
    Using calender java class for time field for tasks. We can use the roll method to change a week/day. Also has a method that lets us choose the first/last day of the month.
    Also added comments for all getters and setters and fields.

--- app/src/main/java/com/example/teh_k/ChoreMate/Task.java
+++ app/src/main/java/com/example/teh_k/ChoreMate/Task.java
@@ -1,60 +1,85 @@
 package com.example.teh_k.ChoreMate;
+// used to keep track of the time of tasks
+import java.util.Calendar;
 // used to create a list of users
 import java.util.List;
 
+// Class that will handle the logic of tasks
 public class Task {
-    //name of the task, set by the user
+    //name of task name and details, set by the user
     private String task_name;
-    //details of the task, set by the user
     private String task_detail;
+
     //whether the task is "triggered" by another user or occurs at set times
+    // 0 for no trigger, 1 for trigger
     private boolean trigger;
-    //recurrence? TODO: figure out what status is
+
+    //whether the task is standby or in progress
+    // 0 for standby, 1 for in progress
     private boolean status;
+
     //list of users that the task will be accomplished by
     private List<User> user_list;
-    // TODO: what is time's type?
-    //private time
 
-    //getter for task name
+    // time that the task is to occur
+    private Calendar time;
+
+    // whether or not the task recurs
+    // 0 for non recurring, 1 for recurring
+    private boolean recur;
+    //TODO: how should we handle when the task recurs, for daily, weekly, monthly, yearly
+
+    //getter and setter for task name and details
     public String getTask_name() {
         return task_name;
     }
-    //setter for task name
     public void setTask_name(String task_name) {
         this.task_name = task_name;
     }
-
-    //getter for task details
     public String getTask_detail() {
         return task_detail;
     }
-    //setter for task details
     public void setTask_detail(String task_detail) {
         this.task_detail = task_detail;
     }
-    //getter for trigger
+
+    //getter and setter for trigger
     public boolean isTrigger() {
         return trigger;
     }
-    //setter for trigger
     public void setTrigger(boolean trigger) {
         this.trigger = trigger;
     }
-    //getter for status
+
+    //getter and setter for status
     public boolean isStatus() {
         return status;
     }
-    //setter for status
     public void setStatus(boolean status) {
         this.status = status;
     }
 
-    //getter for the list of user
+    //getter and setter for the list of user
     public List<User> getUser_list() {
         return user_list;
     }
     public void setUser_list(List<User> user_list) {
         this.user_list = user_list;
+    }
+
+    //getter and setter for the time the task occurs
+    public Calendar getTime() {
+        return time;
+    }
+    public void setTime(Calendar time) {
+        this.time = time;
+    }
+
+    //getter and setter for whether the task recurs
+    public boolean isRecur() {
+        return recur;
+    }
+    public void setRecur(boolean recur) {
+        this.recur = recur;
     }
 }
