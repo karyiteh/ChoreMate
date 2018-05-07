4dd27aaecb34c8ee56b00fb22f7c6d95089092ce
Tan-JamickoJohn <jjt025@ucsd.edu>
2018-05-06 23:41:51 -0700

    Added: Create Household

---
+++ app/src/main/java/com/example/teh_k/ChoreMate/CreateHousehold.java
@@ -0,0 +1,13 @@
+package com.example.teh_k.ChoreMate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateHousehold extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_household);
+    }
+}
