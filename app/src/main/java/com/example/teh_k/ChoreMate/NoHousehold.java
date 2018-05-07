1f774f75b53958cf1a26396b45f8bf011a1dee04
Tan-JamickoJohn <jjt025@ucsd.edu>
2018-05-06 23:03:27 -0700

    Updated login, register. Added: No Household

---
+++ app/src/main/java/com/example/teh_k/ChoreMate/NoHousehold.java
@@ -0,0 +1,13 @@
+package com.example.teh_k.ChoreMate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoHousehold extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_household);
+    }
+}
