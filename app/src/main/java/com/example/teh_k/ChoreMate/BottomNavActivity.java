a275341deea2412366fa49bc38642cd2cbff532f
Kar Yi Teh <teh_karyi97@hotmail.com>
2018-05-09 00:37:02 -0700

    Added bottom nav bar at household activity.
    Signed-off-by: Kar Yi Teh <teh_karyi97@hotmail.com>

--- app/src/main/java/com/example/teh_k/ChoreMate/BottomNavActivity.java
+++ app/src/main/java/com/example/teh_k/ChoreMate/BottomNavActivity.java
@@ -1,5 +1,6 @@
 package com.example.teh_k.ChoreMate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -18,16 +19,22 @@ public class BottomNavActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.action_tasks:
+                    // TODO: Start the tasks activity.
                     mTextMessage.setText(R.string.task_view);
                     return true;
                 case R.id.action_transactions:
+                    // TODO: Start the transactions activity.
                     mTextMessage.setText(R.string.transaction_view);
                     return true;
                 case R.id.action_household:
-                    mTextMessage.setText(R.string.household_view);
+                    // Starts the household activity.
+                    //mTextMessage.setText(R.string.household_view);
+                    householdIntent();
                     return true;
                 case R.id.action_profile:
-                    mTextMessage.setText(R.string.profile_view);
+                    // Starts the user profile activity.
+                    //mTextMessage.setText(R.string.profile_view);
+                    profileIntent();
                     return true;
             }
             return false;
@@ -42,6 +49,22 @@ public class BottomNavActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_profile);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    /**
+     * Starts the household activity.
+     */
+    private void householdIntent() {
+        Intent intent = new Intent(this, HouseholdActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Starts the user profile activity.
+     */
+    private void profileIntent() {
+        Intent intent = new Intent(this, UserProfileActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/teh_k/ChoreMate/HouseholdActivity.java
+++ app/src/main/java/com/example/teh_k/ChoreMate/HouseholdActivity.java
@@ -2,12 +2,60 @@ package com.example.teh_k.ChoreMate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HouseholdActivity extends AppCompatActivity {
+
+    private Toolbar appbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_household);
+
+        // Creates the app bar.
+        appbar = findViewById(R.id.appbar_household);
+        setSupportActionBar(appbar);
+    }
+
+    /**
+     * Creates the options menu on the app bar.
+     * @param menu  The menu item to be passed in.
+     * @return  true if options menu is created successfully.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_household, menu);
+        return true;
+    }
+
+    /**
+     * Handles click events on the menu item selected.
+     * @param item  The menu item that is selected by the user.
+     * @return  true if item is processed accordingly.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        switch(item.getItemId()) {
+            case R.id.action_rename_household:
+                // TODO: Method to rename household.
+                return true;
+            case R.id.action_delete_household:
+                // TODO: Method to delete household.
+                return true;
+            case R.id.action_invite_housemate:
+                // TODO: Method to invite housemate.
+                return true;
+            case R.id.action_remove_housemate:
+                // TODO: Method to remove housemate.
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
