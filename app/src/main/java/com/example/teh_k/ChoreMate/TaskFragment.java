bbc2894a8fe1aa34e54f61f240d952974b1325ed
Kar Yi Teh <teh_karyi97@hotmail.com>
2018-05-12 21:36:20 -0700

    Changed TaskViewActivity to TaskFragment.java
    Signed-off-by: Kar Yi Teh <teh_karyi97@hotmail.com>

---
+++ app/src/main/java/com/example/teh_k/ChoreMate/TaskFragment.java
@@ -0,0 +1,30 @@
+package com.example.teh_k.ChoreMate;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Displays the tasks in MainActivity.java.
+ */
+public class TaskFragment extends Fragment {
+
+
+    public TaskFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_view, container, false);
+    }
+
+}
