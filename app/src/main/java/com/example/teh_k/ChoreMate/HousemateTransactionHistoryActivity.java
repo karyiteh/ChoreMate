9964513d1f7056fb0ef7bf6563f448315c2954fc
Tan-JamickoJohn <jjt025@ucsd.edu>
2018-05-20 18:16:59 -0700

    Edit:     View task:         Gravity for 4 buttons pulled down     Create Task:         Put background
    Added :
        Payment UI's and rows

---
+++ app/src/main/java/com/example/teh_k/ChoreMate/HousemateTransactionHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.teh_k.ChoreMate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HousemateTransactionHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_housemate_transaction_history);
+    }
+}
---
+++ app/src/main/java/com/example/teh_k/ChoreMate/PaymentFragment.java
@@ -0,0 +1,13 @@
+package com.example.teh_k.ChoreMate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_payment_view);
+    }
+}
